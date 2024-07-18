ecccca33221ea9837fe6572c9fdef9e75cda3335
Alissa Lima <alissa.lima18@gmail.com>
2024-07-17 23:30:38 -0300

    Testes de tela

--- TrabalhoBaseDefense/TrabalhoBaseDefense.cpp
+++ TrabalhoBaseDefense/TrabalhoBaseDefense.cpp
@@ -26,32 +26,41 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
 
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    heroi.setMunicao(heroi.getMunicao() - 1);
+                }
+            }
+
         }
 
-        sf::Font font = sf::Font::get;
+        sf::Font font;
+        font.loadFromFile("resources/font/arial.ttf");
 
         std::string vida = "Vida: " + std::to_string(heroi.getVida());
         sf::Text textVida;
-        textVida.setFont(sf::Font::);
-        textVida.setPosition(250, 200);
+        textVida.setFont(font);
+        textVida.setPosition(1100, 20);
         textVida.setString(vida);
-        textVida.setCharacterSize(50);
+        textVida.setCharacterSize(24);
         textVida.setFillColor(sf::Color::Black);
         textVida.setStyle(sf::Text::Bold);
 
+
         std::string municao = "Municao: " + std::to_string(heroi.getMunicao());
         sf::Text textMunicao;
-        textMunicao.setPosition(100, 150);
+        textMunicao.setFont(font);
+        textMunicao.setPosition(1100, 50);
         textMunicao.setString(municao);
         textMunicao.setCharacterSize(24);
         textMunicao.setFillColor(sf::Color::Black);
-        textVida.setStyle(sf::Text::Bold);
-
-
-        window.draw(textVida);
-        window.draw(textMunicao);
+        textMunicao.setStyle(sf::Text::Bold);
 
         window.clear(sf::Color::White);
+
+        window.draw(textMunicao);
+        window.draw(textVida);
+
         window.display();
 
     }
