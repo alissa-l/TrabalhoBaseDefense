98f5eeb9ff51aedd072137fcc87475888cb1b12e
Alissa Lima <alissa.lima18@gmail.com>
2024-07-23 00:41:27 -0300

    Inimigos atiram

--- TrabalhoBaseDefense/TrabalhoBaseDefense.cpp
+++ TrabalhoBaseDefense/TrabalhoBaseDefense.cpp
@@ -24,6 +24,7 @@ sf::RectangleShape heroiShape;
 // Inimigos
 std::vector<Inimigo> inimigos;
 float inimigoSpeed = 2.0f;
+int inimigo_freq = 360;
 
 // Projeteis
 std::vector<Projetil> projeteis;
@@ -49,6 +50,8 @@ void spawnInimigo();
 void calcKill();
 
 void calcDano();
+
+void draws(sf::RenderWindow &window);
 
 int main() {
     // Janela
@@ -94,36 +97,39 @@ int main() {
         update(window);
 
         // DRAW
-        window.draw(heroiShape);
-
-        for (int(i) = 0; i < projeteis.size(); i++) {
-            window.draw(projeteis[i].getProjetil());
-        }
-
-        for(int i = 0; i < inimigos.size(); i++) {
-            std::cout << inimigos[i].getInimigoShape().getPosition().x << " " << inimigos[i].getInimigoShape().getPosition().y << std::endl;
-            window.draw(inimigos[i].getInimigoShape());
-        }
-
-        sf::RectangleShape base = sf::RectangleShape(sf::Vector2f(400, 300));
-        base.setOutlineThickness(5);
-        base.setFillColor(sf::Color::Transparent);
-        base.setOutlineColor(sf::Color::Black);
-        base.setPosition(440, 210);
-
-        window.draw(base);
-
-        std::string vida = "Vida: " + std::to_string(heroi.getVida());
-        DrawUtils::createText(vida, 1100, 20, window);
-
-        std::string municao = "Municao: " + std::to_string(heroi.getMunicao());
-        DrawUtils::createText(municao, 1100, 50, window);
+        draws(window);
 
         window.display();
 
     }
 
     return 0;
+}
+
+void draws(sf::RenderWindow &window) {
+    window.draw(heroiShape);
+
+    for (int(i) = 0; i < projeteis.size(); i++) {
+        window.draw(projeteis[i].getProjetil());
+    }
+
+    for(int i = 0; i < inimigos.size(); i++) {
+        window.draw(inimigos[i].getInimigoShape());
+    }
+
+    sf::RectangleShape base = sf::RectangleShape(sf::Vector2f(400, 300));
+    base.setOutlineThickness(5);
+    base.setFillColor(sf::Color::Transparent);
+    base.setOutlineColor(sf::Color::Black);
+    base.setPosition(440, 210);
+
+    window.draw(base);
+
+    std::string vida = "Vida: " + std::to_string(heroi.getVida());
+    DrawUtils::createText(vida, 1100, 20, window);
+
+    std::string municao = "Municao: " + std::to_string(heroi.getMunicao());
+    DrawUtils::createText(municao, 1100, 50, window);
 }
 
 void update(sf::RenderWindow &window) {
@@ -150,13 +156,14 @@ void calcKill() {
 
     for(int i = 0; i < projeteis.size(); i++) {
         for(int j = 0; j < inimigos.size(); j++) {
-            if(projeteis[i].getProjetil().getGlobalBounds().intersects(inimigos[j].getInimigoShape().getGlobalBounds())) {
-                projeteis.erase(projeteis.begin() + i);
-                inimigos[j].setVida(inimigos[j].getVida() - 100);
-                if(inimigos[j].getVida() <= 0) {
-                    inimigos.erase(inimigos.begin() + j);
-                }
-            }
+//            if(projeteis[i].getProjetil().getGlobalBounds().intersects(inimigos[j].getInimigoShape().getGlobalBounds())) {
+//                projeteis.erase(projeteis.begin() + i);
+//                inimigos[j].setVida(inimigos[j].getVida() - 100);
+//                if(inimigos[j].getVida() <= 0) {
+//                    inimigos.erase(inimigos.begin() + j);
+//                    heroi.setMunicao(heroi.getMunicao() + 2);
+//                }
+//            }
         }
     }
 
@@ -185,7 +192,6 @@ void calcShoot(sf::RenderWindow &window) {
 
         if (newProj.getPosition().x < 0 || newProj.getPosition().x > tamX || newProj.getPosition().y < 0 ||
             newProj.getPosition().y > tamY) {
-            std::cout << "Removendo projétil" << std::endl;
             projeteis.erase(projeteis.begin() + i);
         }
     }
@@ -211,7 +217,10 @@ void moveHero(sf::RenderWindow &window) {
 
 // Cuida da lógica de spawn e movimento dos inimigos
 void spawnInimigo() {
-    if(frame_count % 120 == 0) {
+    if(frame_count % inimigo_freq == 0) {
+        if(frame_count > 60) {
+            inimigo_freq -= 10;
+        }
         sf::RectangleShape inimigoShape = sf::RectangleShape(sf::Vector2f(50, 50));
 
         int x;
@@ -236,12 +245,14 @@ void spawnInimigo() {
         sf::RectangleShape inimigoShape = inimigos[i].getInimigoShape();
         sf::Vector2f direcao = VectorUtils::calcularDirecao(inimigoShape.getPosition(), heroiShape.getPosition());
         inimigoShape.setPosition(inimigoShape.getPosition() + direcao * inimigoSpeed);
+        inimigos[i].setInimigoShape(inimigoShape);
+    }
 
-        if(inimigoShape.getGlobalBounds().intersects(heroiShape.getGlobalBounds())) {
-            continue;
-        } else {
-            inimigos[i].setInimigoShape(inimigoShape);
-        }
-
+    for(int i = 0; i < inimigos.size(); i++) {
+        sf::RectangleShape projShape = sf::RectangleShape(sf::Vector2f(10, 10));
+        projShape.setFillColor(sf::Color::Black);
+        projShape.setPosition(inimigos[i].getInimigoShape().getPosition());
+        Projetil proj = Projetil(VectorUtils::calcularDirecao(inimigos[i].getInimigoShape().getPosition(), heroiShape.getPosition()), projShape);
+        projeteis.push_back(proj);
     }
 }
